a240d860899227e9f4d03777ce4bb0320e4ab109
Gabriela Vargas <gvargas@sugvargas.mdlive.local>
2018-01-31 10:39:42 -0500

    created place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,6 @@
 class Place < ApplicationRecord
+  scope :by_longitude, -> (min, max) { min && max ? where('longitude >= :min AND longitude <= :max', min: min, max: max) :all }
+  scope :by_latitude, -> (min, max) { min && max ? where('latitude >= :min AND latitude <= :max', min: min, max: max) :all }
+  
+  API_RESULTS_LIMIT = 100
 end
