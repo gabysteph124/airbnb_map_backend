8a60f631e00faa1dae4250386ea33e8686e2dc97
Gabriela Vargas <gvargas@sugvargas.mdlive.local>
2018-02-02 11:12:40 -0500

    cleaned code

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,9 +3,9 @@ class PlacesController < ApplicationController
     places = Place.search(search_params.to_h.symbolize_keys)
     render json: places
   end
-  
+
   private
-  
+
   def search_params
     params.permit(:min_lng, :max_lng, :min_lat, :max_lat)
   end
